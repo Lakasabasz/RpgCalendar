2fb11c5d791181689c31d670aaebcdda0e5f8c85
Lakasabasz <lakasabasz@gmail.com>
2024-08-18 23:18:15 +0200

    Add authorization and API routing to placeholders in CalendarController.

---
+++ RpgCalendar.API/Controllers/Users/CalendarController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RpgCalendar.API.Controllers.Users;
+
+[Authorize]
+[ApiController, Route("/users/{userId:guid}/calendar")]
+public class CalendarController : CustomController
+{
+    [HttpGet("absences")]
+    public IActionResult GetAbsences()
+    {
+        return Ok();
+    }
+
+    [HttpPost("absences")]
+    public IActionResult AddAbsence()
+    {
+        return Ok();
+    }
+
+    [HttpDelete("absences/{absenceId:guid}")]
+    public IActionResult DeleteAbsence()
+    {
+        return Ok();
+    }
+    
+    [HttpGet("events")]
+    public IActionResult GetEvents()
+    {
+        return Ok();
+    }
+
+    [HttpPost("events")]
+    public IActionResult AddEvent()
+    {
+        return Ok();
+    }
+    
+    [HttpPatch("events/{eventId:guid}")]
+    public IActionResult UpdateEvent()
+    {
+        return Ok();
+    }
+    
+    [HttpDelete("events/{eventId:guid}")]
+    public IActionResult DeleteEvent()
+    {
+        return Ok();
+    }
+}
