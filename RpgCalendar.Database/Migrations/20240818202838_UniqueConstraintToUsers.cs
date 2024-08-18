8036eef75e3e1a82b7abb2cabd9fab71d654b179
Lakasabasz <lakasabasz@gmail.com>
2024-08-18 22:30:41 +0200

    Add unique constraint to User's PrivateCode field

---
+++ RpgCalendar.Database/Migrations/20240818202838_UniqueConstraintToUsers.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RpgCalendar.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniqueConstraintToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_PrivateCode",
+                table: "Users",
+                column: "PrivateCode",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_PrivateCode",
+                table: "Users");
+        }
+    }
+}
--- RpgCalendar.Database/Models/User.cs
+++ RpgCalendar.Database/Models/User.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace RpgCalendar.Database.Models;
 
+[Index(nameof(PrivateCode), IsUnique = true)]
 public class User
 {
     [Key] public Guid Id { get; set; }
@@ -9,4 +11,11 @@ public class User
     [MaxLength(64)] public string Nick { get; set; }
     
     [MaxLength(6)] public string PrivateCode { get; set; }
+    
+    public static User Prepare(Guid id, string nick, string privateCode) => new()
+    {
+        Id = id,
+        Nick = nick,
+        PrivateCode = privateCode
+    };
 }
