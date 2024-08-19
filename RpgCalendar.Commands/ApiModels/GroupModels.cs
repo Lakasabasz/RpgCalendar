b019a4fbbb2c3c8621b488a21f903fad30f3691b
Lakasabasz <lakasabasz@gmail.com>
2024-08-19 21:00:46 +0200

    Response models

---
+++ RpgCalendar.Commands/ApiModels/GroupModels.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using RpgCalendar.Tools;
+
+namespace RpgCalendar.Commands.ApiModels;
+
+public record GroupsList(IEnumerable<GroupShort> Groups): IApiResponse;
+
+public record GroupShort(Guid Id, string Name, string ProfilePicture, DateTime CreatedAt);
+
+public record GroupFull(Guid Id, string Name, string ProfilePicture, DateTime CreatedAt): IApiResponse;
+
+public record MembersList(IEnumerable<Member> Members) : IApiResponse;
+
+public record Member(Guid MemberId, string DisplayName);
+
+public record ExternalInvite(Guid InviteId): IApiResponse;
