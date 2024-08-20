8fa791155aaf678b9cfdb252095df009e3d5b182
Lakasabasz <lakasabasz@gmail.com>
2024-08-20 12:09:43 +0200

    Implement GET /groups

---
+++ RpgCalendar.Commands/Jobs/Groups/GetGroupsJob.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RpgCalendar.Commands.ApiModels;
+using RpgCalendar.Database;
+using RpgCalendar.Tools;
+
+namespace RpgCalendar.Commands.Jobs.Groups;
+
+public class GetGroupsJob(RelationalDb db, ImageService imageService): IJob
+{
+    public record JobData(Guid userId);
+
+    public ErrorCode? Error => null;
+    public IApiResponse? ApiResponse { get; private set; }
+
+    public void Execute(JobData data)
+    {
+        var groups = db.GroupsMembers.Include(x => x.Group)
+            .Where(x => x.UserId == data.userId)
+            .Select(x => new GroupShort(x.Group.GroupId, x.Group.Name,
+                imageService.GetImageUrl(x.Group.ProfilePicture), x.Group.CreationDate));
+        ApiResponse = new GroupsList(groups);
+    }
+}
