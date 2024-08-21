cb4815cfce71bd7546873783edac8f460f5a0000
Lakasabasz <lakasabasz@gmail.com>
2024-08-21 16:00:28 +0200

    Remove invites before remove group

--- RpgCalendar.Commands/Jobs/Groups/DeleteGroupJob.cs
+++ RpgCalendar.Commands/Jobs/Groups/DeleteGroupJob.cs
@@ -12,8 +12,10 @@ public class DeleteGroupJob(RelationalDb db): IJob
     public void Execute(JobData data)
     {
         var members = db.GroupsMembers.Where(x => x.GroupId == data.groupId);
+        var invites = db.GroupsInvites.Where(x => x.GroupId == data.groupId);
         var group = db.Groups.First(x => x.GroupId == data.groupId);
         db.GroupsMembers.RemoveRange(members);
+        db.GroupsInvites.RemoveRange(invites);
         db.Groups.RemoveRange(group);
         db.SaveChanges();
     }
