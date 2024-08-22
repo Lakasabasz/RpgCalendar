13120f1b6377a6b375b0344e97f807e6cc015ae5
Lakasabasz <lakasabasz@gmail.com>
2024-08-22 18:03:42 +0200

    Implement PATCH /groups/{groupId:guid}/members/{memberId:guid}/permission

---
+++ RpgCalendar.API/Controllers/Groups/Members/MemberController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RpgCalendar.API.Requests;
+using RpgCalendar.Commands;
+using RpgCalendar.Commands.Jobs.Groups.Members;
+using RpgCalendar.Tools;
+
+namespace RpgCalendar.API.Controllers.Groups.Members;
+
+[ApiController]
+[Authorize, Route("groups/{groupId:guid}/members/{memberId:guid}")]
+class MemberController(AccessTester tester,
+    Lazy<PatchMemberPermissionJob> patchMemberPermission): CustomController
+{
+    [HttpPatch("permission")]
+    public IActionResult UpdateGroupMemberPermission([FromRoute] Guid groupId, [FromRoute] Guid memberId, 
+                                                     [FromBody] UpdateGroupPermission payload)
+    {
+        if (payload.Permission == PermissionLevel.Owner) return EarlyError(ErrorCode.CannotSetOwnerPermission);
+        if (Invoker is null) return EarlyError(ErrorCode.UserNotRegistered);
+        if (tester.TestIf(Invoker).HasAccessTo.Group(groupId).Manage()) return Forbid();
+        
+        patchMemberPermission.Value.Execute(new PatchMemberPermissionJob.JobData(groupId, memberId, payload.Permission));
+        return HandleJobResult(patchMemberPermission.Value);
+    }
+}
---
+++ RpgCalendar.Commands/Jobs/Groups/Members/PatchMemberPermissionJob.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RpgCalendar.Commands.ApiModels;
+using RpgCalendar.Database;
+using RpgCalendar.Tools;
+
+namespace RpgCalendar.Commands.Jobs.Groups.Members;
+
+public class PatchMemberPermissionJob(RelationalDb db): IJob
+{
+    public record JobData(Guid GroupId, Guid MemberId, PermissionLevel Permission);
+
+    public ErrorCode? Error { get; private set; }
+    public IApiResponse? ApiResponse { get; private set; }
+
+    public void Execute(JobData data)
+    {
+        var member = db.GroupsMembers.FirstOrDefault(x => x.GroupId == data.GroupId && x.UserId == data.MemberId);
+        if(member is null)
+        {
+            Error = ErrorCode.MemberNotInGroup;
+            return;
+        }
+        if(member.PermissionLevel == PermissionLevel.Owner)
+        {
+            Error = ErrorCode.CannotChangeOwnerPermission;
+            return;
+        }
+
+        member.PermissionLevel = data.Permission;
+        db.SaveChanges();
+        var members = db.GroupsMembers
+            .Where(x => x.GroupId == data.GroupId)
+            .Include(x => x.User)
+            .Select(x => new Member(x.UserId, x.User.Nick, x.PermissionLevel));
+        ApiResponse = new MembersList(members);
+    }
+}
