edeed3e78992d7299abb6d4f623df1884cc6c820
Lakasabasz <lakasabasz@gmail.com>
2024-08-22 18:24:11 +0200

    Implement PATCH /groups/{groupId:guid}/members/{memberId:guid}/ownership

--- RpgCalendar.API/Controllers/Groups/Members/MemberController.cs
+++ RpgCalendar.API/Controllers/Groups/Members/MemberController.cs
@@ -10,7 +10,8 @@ namespace RpgCalendar.API.Controllers.Groups.Members;
 [ApiController]
 [Authorize, Route("groups/{groupId:guid}/members/{memberId:guid}")]
 class MemberController(AccessTester tester,
-    Lazy<PatchMemberPermissionJob> patchMemberPermission): CustomController
+    Lazy<PatchMemberPermissionJob> patchMemberPermission,
+    Lazy<TransferOwnershipJob> transferOwnership): CustomController
 {
     [HttpPatch("permission")]
     public IActionResult UpdateGroupMemberPermission([FromRoute] Guid groupId, [FromRoute] Guid memberId, 
@@ -22,5 +23,15 @@ class MemberController(AccessTester tester,
         
         patchMemberPermission.Value.Execute(new PatchMemberPermissionJob.JobData(groupId, memberId, payload.Permission));
         return HandleJobResult(patchMemberPermission.Value);
+    }
+    
+    [HttpPatch("ownership")]
+    public IActionResult TransferOwnership([FromRoute] Guid groupId, [FromRoute] Guid memberId)
+    {
+        if (Invoker is null) return EarlyError(ErrorCode.UserNotRegistered);
+        if (tester.TestIf(Invoker).HasAccessTo.Group(groupId).Ownership()) return Forbid();
+        
+        transferOwnership.Value.Execute(new TransferOwnershipJob.JobData(groupId, memberId));
+        return HandleJobResult(transferOwnership.Value);
     }
 }
