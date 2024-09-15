01dacc620d9a6fff459fcd48bab7fcf75a6bc7fe
Lakasabasz <lakasabasz@gmail.com>
2024-09-15 21:27:30 +0200

    Implement time range validator

---
+++ RpgCalendar.API/Validation/ITimeRangeValidation.cs
@@ -0,0 +1,15 @@
+﻿namespace RpgCalendar.API.Validation;
+
+public interface ITimeRangeValidation
+{
+    DateTime Begin { get; }
+    DateTime End { get; }
+}
+
+public static class TimeRangeValidationExtensions
+{
+    public static bool ValidateTimeRange(this ITimeRangeValidation validation)
+    {
+        return validation.Begin <= validation.End;
+    }
+}
