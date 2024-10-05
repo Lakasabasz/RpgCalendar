0dcc2402c8209c317d903902276a0b5469044883
Lakasabasz <lakasabasz@gmail.com>
2024-10-05 22:08:17 +0200

    Fix missing migrations

---
+++ RpgCalendar.Database/Migrations/20241005200622_FixRenamedFields.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RpgCalendar.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixRenamedFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Start",
+                table: "PrivateEvents",
+                newName: "StartTime");
+
+            migrationBuilder.RenameColumn(
+                name: "End",
+                table: "PrivateEvents",
+                newName: "EndTime");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StartTime",
+                table: "PrivateEvents",
+                newName: "Start");
+
+            migrationBuilder.RenameColumn(
+                name: "EndTime",
+                table: "PrivateEvents",
+                newName: "End");
+        }
+    }
+}
